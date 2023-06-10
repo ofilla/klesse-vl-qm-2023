8b709c4d0f338d1782a78131e2a78061132f056b
Oliver Filla <ocl@hotmail.de>
2023-06-10 12:42:30 +0200

    Fix export script

--- templates/export.sh
+++ templates/export.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cat Mitschrift/*.md > Quantenmechanik.md || exit $?
 sed -i 's/braket/expval/g' Quantenmechanik.md
 sed -i "s/Sommersemester 2023/$(date +%d.%M.%Y)/" Quantenmechanik.md
 sed -i '/Dr. Rochus Klesse/d' Quantenmechanik.md
